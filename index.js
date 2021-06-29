c95f0f94d7fc32405d04908d452b8cbe9742128d
musty <mustafaumar70@live.com>
2021-06-29 21:51:05 +0100

    changes

--- index.js
+++ index.js
@@ -1,20 +1,30 @@
 const fs = require('fs');
+const express = require('express');
+const app = express();
+
+const http = require('http').createServer(app);
+
 
 //the csv-parse package from the node-csv is required to read CSV files
 const parse = require('csv-parse')
+const Papa = require('papaparse');
+const csv = require('fast-csv')
 
 
+
+var data = [];
 //
 const parser = parse({columns: true}, (err,records) => {
-    var newHeaderData = [];
+    var bodyData = [];
 
     var itemArray = [];
 
     var headerArray = [];
 
-    var BodyArray = [];
+    var newBodyData = [];
 
     var ColInQuestionData = [];
+
     
     
   
@@ -22,6 +32,11 @@ const parser = parse({columns: true}, (err,records) => {
       records.map(item => {
 
         //console.log(Object.keys(item))
+        //console.log(Object.values(item))
+        const body =  Object.values(item)
+
+        //save body data
+        bodyData.push(...body)
 
         const values = Object.values(item)[Object.values(item).length-1]
         //console.log(values);
@@ -45,6 +60,7 @@ const parser = parse({columns: true}, (err,records) => {
             
             
 
+    //add new headers
     headerArray.push(...itemArray)
     //console.log(ColInQuestionData.length)
 
@@ -52,20 +68,27 @@ const parser = parse({columns: true}, (err,records) => {
            ColInQuestionData.map(item => {
        // const b = ColInQuestionData.includes(i)
        if(item.match(i)){
-        console.log('1')
+        //console.log('1')
+        newBodyData.push('1')
        } else {
-           console.log('2')
+        newBodyData.push('2')
        }
        //console.log(item + '********')
     })
-    console.log(i + '===================================')
+   // console.log(i + '===================================')
     })
  
 
     //check if book exists
 
+    //append data
 
+    bodyData.push(...newBodyData)
+    //console.log(bodyData)
 
+    data = [headerArray, bodyData]
+    const csv = Papa.unparse(data);
+    console.log(data)
 
  
 })
@@ -74,3 +97,22 @@ const parser = parse({columns: true}, (err,records) => {
 //
 fs.createReadStream(__dirname+'/task-data.csv').pipe(parser);
 
+
+
+
+app.use("/public", express.static(__dirname + "/public"));
+
+
+//start server
+http.listen(3000, () => {
+    console.log("server connected")
+
+    app.get("/", (req, res) => {
+
+        const ws = fs.createWriteStream("public/task-output.csv");
+        csv.write(data, {headers: true}).on("finish", () => {
+            res.send("send");
+        })
+        
+    })
+})
