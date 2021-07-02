b447b83ddc923f8ab16388b45438dccb2846620b
musty <mustafaumar70@live.com>
2021-07-02 11:43:29 +0100

    m

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const fs = require('fs');
-const parse = require('csv-parse')
+
 const Papa = require('papaparse');
 
 
@@ -10,7 +10,9 @@ const parseStream = Papa.parse(Papa.NODE_STREAM_INPUT)
 dataStream.pipe(parseStream)
 
 
-let data = [];
+
+//** DECLARE OUR ARRAYS
+let initialData = [];
 
 let headerArray = [];
 
@@ -18,40 +20,46 @@ let rowsData = [];
 
 let newColumns = [];
 
-let finalData = [];
+let csvData = [];
+
+
 
 
 
 
 
 parseStream.on("data", chunk => {
-    data.push(chunk)
+    initialData.push(chunk)
 })
 
 parseStream.on("finish", () => {
   
-
+//**STEP 1 */
     //Initialize the headers with the data[0], which is the array that holds the header columns
-    headerArray = data[0];
+    headerArray = initialData[0];
 
-    //Initialize the rowsData with
-    rowsData = data.slice(1)
+    //Initialize the rowsData with data.slice(1). This is going to give us an array of arrays that hold the data of the rows
+    rowsData = initialData.slice(1)
     
-    //Find Column in Question
-    //To do this, we can find it using Index of the column  in question
+//**STEP 2  - Find Column in Question*/
+   
+    //To do this, we will use the 'ColInQuestion' index
     colInQuestionIndex = headerArray.indexOf('ColInQuestion');
     
     //Then we retrieve the data of each row for the Column in Question. 
     rowsData.map(data => {
     
-    //We get the data for each row of the column in question
+    //We get the data for each row of the column in question using the colInQuestionIndex
         const colInQuestionRowData = data[colInQuestionIndex]
 
-    //The we use the data to generate the additional header columns. when doing this, we make sure we avoding adding duplicates to the header array
+    //The we use the data to generate the additional header columns. when doing this, we make sure we avoid adding duplicates to the header array
+
         colInQuestionRowData.split(";").map(data => {
 
+            // The newColumns array will help us assign data - 1(picked) or 2(not picked) to our additional columns.
             if (newColumns.indexOf(data) === -1) newColumns.push(data) 
 
+            //Then we add our unique set of additional column headers to the initial headers
             if (headerArray.indexOf(data) === -1) headerArray.push(data) 
           
         })
@@ -61,18 +69,15 @@ parseStream.on("finish", () => {
     //console.log(newColumns)
 
 
-    // Add 2 (not picked) to the row data for the new columns
-    
-        
-        //check if the new columns is picked(1) or not picked(2)
+//**STEP 3 - populate our newly created columns with the appropriate data. */ // 
         newColumns.map(column => {
-
             rowsData.map(data => {
                 const colInQuestionRowData = data[colInQuestionIndex]
-
+// if the column is picked, we add 1 as the column value
                 if(colInQuestionRowData.match(column)){
                      data.push('1')
                 } else {
+// if the column isn't picked, we add 2 as the column value
                     data.push('2')
                 }
             
@@ -82,16 +87,13 @@ parseStream.on("finish", () => {
         })
         
      
- 
-  
-    console.log(data)
-    //console.log(rowsData)
-    //console.log(headerArray.length)
-    
 
-    //unparse the file and create the output
-       const csv = Papa.unparse(rowsData);
-        fs.createWriteStream(__dirname+'/task-output.csv').write(csv)
+
+    
+//**LAST STEP - unparse the data and generate the output file */
+
+       const transformedData = Papa.unparse({fields: headerArray, data:rowsData})
+        fs.createWriteStream(__dirname+'/task-output.csv').write(transformedData)
    
   
 })
